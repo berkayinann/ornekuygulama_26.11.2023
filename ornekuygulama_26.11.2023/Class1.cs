478cd93f36d98a1a77920797a1cb02a1f116b8d5
Tarkovski7 <trkbgr1618@gmail.com>
2023-11-27 00:00:48 +0300

    asdsad

--- ornekuygulama_26.11.2023/Class1.cs
+++ ornekuygulama_26.11.2023/Class1.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ornekuygulama_26._11._2023
+﻿namespace ornekuygulama_26._11._2023
 {
     internal class Class1
     {
+        //asdasdasdasda
     }
 }
--- ornekuygulama_26.11.2023/Tarik.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ornekuygulama_26._11._2023
-{
-    public class Tarik
-    {
-        public int MyProperty { get; set; }
-        public int MyProperty1 { get; set; }
-
-    }
-}
